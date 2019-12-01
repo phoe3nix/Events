bc5b09f297964441806cd626a7d3b2113a7731d2
Vladislav Usachev <Usachov.V.An@sberbank.ru>
2019-12-01 21:08:34 +0300

    Initial Commit

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Владислав Усачёв on 01/12/2019.
+//  Copyright © 2019 Vladislav Usachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
