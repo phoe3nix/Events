bc21004e773d23d4cd8e2d40596a920d6f464a9d
Vladislav Usachev <Usachov.V.An@sberbank.ru>
2019-12-02 22:03:16 +0300

    проверка того, что билд падает

---
+++ EventsTests/EventsTests.swift
@@ -0,0 +1,20 @@
+//
+//  EventsTests.swift
+//  EventsTests
+//
+//  Created by Владислав Усачёв on 02/12/2019.
+//  Copyright © 2019 Vladislav Usachev. All rights reserved.
+//
+
+import XCTest
+
+class EventsTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
